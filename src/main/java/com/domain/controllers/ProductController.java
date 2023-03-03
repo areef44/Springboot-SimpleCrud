3380947852651d3fcee75bd98690df719db7b3b3
areef44 <areef.empat.empat@gmail.com>
2023-03-03 09:57:26 +0700

    (+)Update Validation

--- src/main/java/com/domain/controllers/ProductController.java
+++ src/main/java/com/domain/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.domain.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.domain.dto.ResponseData;
 import com.domain.models.entities.Product;
 import com.domain.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/products")
@@ -21,8 +28,21 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public Product create(@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity<ResponseData<Product>> create(@Valid @RequestBody Product product, Errors errors){
+        
+        ResponseData<Product> responseData = new ResponseData<>();
+
+        if(errors.hasErrors()){
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @GetMapping
@@ -36,8 +56,20 @@ public class ProductController {
     }
 
     @PutMapping
-    public Product update(@RequestBody Product product){
-        return productService.save(product);
+    public ResponseEntity<ResponseData<Product>> update(@Valid @RequestBody Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<>();
+
+        if(errors.hasErrors()){
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayload(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/domain/models/entities/Product.java
+++ src/main/java/com/domain/models/entities/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name= "tbl_product")
@@ -20,9 +21,11 @@ public class Product implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "Name is required")
     @Column(name="product_name", length=100)
     private String name;
 
+    @NotEmpty(message = "Description is required")
     @Column(name="product_description", length=500)
     private String description;
 
